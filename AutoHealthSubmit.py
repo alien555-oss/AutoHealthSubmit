9f67232a51faec212eac5bea9915b0cb62038137
Windmill-City <1449182174@qq.com>
2021-05-04 08:34:09 +0800

    custom mail action

--- AutoHealthSubmit.py
+++ AutoHealthSubmit.py
@@ -60,11 +60,17 @@ def get_chrome_driver():
     return webdriver.Chrome(options=options)
 
 
-def save_for_email(message):
-    # 保存email内容
-    with open("email.txt", 'a+', encoding="utf-8") as email:
+def save_for(file, message):
+    with open(file + ".txt", 'a+', encoding="utf-8") as email:
         email.write(message + '\n')
+        email.flush()
+        email.close()
 
+def save_for_mail_body(message):
+    save_for("body", message)
+
+def save_for_mail_subject(message):
+    save_for("subject", message)
 
 driver = get_chrome_driver()
 driver.set_page_load_timeout(60)
@@ -82,13 +88,14 @@ try:
             submit(driver)
         if is_completed(driver):
             completed = True
+            save_for_mail_body("Completed!")
 except Exception as e:
     logger.error("Failed to submit:\n%s\n" % traceback.format_exc())
-    save_for_email("打卡失败\n%s\n" % traceback.format_exc())
+    save_for_mail_body("打卡失败\n%s\n" % traceback.format_exc())
 driver.quit()
 if completed:
     logger.info("User:%s completed" % usr_id)
-    save_for_email("%s健康申报---成功---！" % usr_id)
+    save_for_mail_subject("健康申报：成功！\n%s" % usr_id)
 else:
     logger.info("User:%s failed" % usr_id)
-    save_for_email("%s健康申报----失败----！" % usr_id)
+    save_for_mail_subject("健康申报：失败！\n%s" % usr_id)
